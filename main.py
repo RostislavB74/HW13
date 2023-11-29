b52c92bbd213bdda6726b8b88499b34c0680c295
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-11-29 22:41:28 +0200

    edit

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ from starlette.middleware.cors import CORSMiddleware
 from fastapi_limiter import FastAPILimiter
 
 from src.database.db import get_db
-
-from src.database.db import get_db
-from src.routes import contacts, auth
+from src.routes import contacts, auth, users
 from src.conf.config import settings
 
 app = FastAPI()
 
+
+# @app.on_event("startup")
+# async def startup():
+#     r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
+#     await FastAPILimiter.init(r)
 @app.on_event("startup")
 async def startup():
     r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
@@ -27,14 +30,15 @@ async def startup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://127.0.0.1:5500', 'http://localhost:5500'],
+    allow_origins=['http://127.0.0.1:8000', 'http://localhost:8000'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-ALLOWED_IPS = [ip_address('192.168.1.0'), ip_address('172.16.0.0'), ip_address("127.0.0.1")]
+ALLOWED_IPS = [ip_address('192.168.1.0'), ip_address(
+    '172.16.0.0'), ip_address("127.0.0.1")]
 
 
 @app.middleware("http")
@@ -70,12 +74,15 @@ def healthchecker(db: Session = Depends(get_db)):
         # Make request
         result = db.execute(text("SELECT 1")).fetchone()
         if result is None:
-            raise HTTPException(status_code=500, detail="Database is not configured correctly")
+            raise HTTPException(
+                status_code=500, detail="Database is not configured correctly")
         return {"message": "Welcome to FastAPI!"}
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=500, detail="Error connecting to the database")
+        raise HTTPException(
+            status_code=500, detail="Error connecting to the database")
 
 
 app.include_router(contacts.router, prefix="/api")
 app.include_router(auth.router, prefix='/api')
+app.include_router(users.router, prefix='/api')
--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -1,6 +1,6 @@
 from typing import List
 from datetime import date, timedelta
-
+from fastapi_limiter.depends import RateLimiter
 from fastapi import Depends, HTTPException, status, Path, APIRouter
 from sqlalchemy.orm import Session
 from src.database.models import User, Role
@@ -17,7 +17,7 @@ allowed_operation_update = RoleAccess([Role.admin, Role.moderator])
 allowed_operation_remove = RoleAccess([Role.admin])
 
 
-@router.get("/", response_model=List[ContactResponse], dependencies=[Depends(allowed_operation_get)], name="Return contacts")
+@router.get("/", response_model=List[ContactResponse], dependencies=[Depends(allowed_operation_get), Depends(RateLimiter(times=2, seconds=5))], name="Return contacts")
 async def get_contacts(db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     contacts = await repository_contacts.get_contacts(db)
     return contacts
@@ -27,43 +27,50 @@ async def get_contacts(db: Session = Depends(get_db), current_user: User = Depen
 async def get_contact(id: int, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.get_contact_by_id(id, db)
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return contact
 
 
 @router.get(
     "/search_by_last_name/{last_name}",
-    response_model=List[ContactResponse], dependencies=[Depends(allowed_operation_get)],
+    response_model=List[ContactResponse], dependencies=[
+        Depends(allowed_operation_get)],
     name="Search contacts by last name",
 )
 async def search_contacts_by_last_name(last_name: str, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.search_contacts_by_last_name(last_name, db)
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return contact
 
 
 @router.get(
     "/search_by_first_name/{first_name}",
-    response_model=List[ContactResponse], dependencies=[Depends(allowed_operation_get)],
+    response_model=List[ContactResponse], dependencies=[
+        Depends(allowed_operation_get)],
     name="Search contacts by first name",
 )
 async def search_contacts_by_first_name(first_name: str, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.search_contacts_by_first_name(first_name, db)
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return contact
 
 
 @router.get(
     "/search_by_email/{email}",
-    response_model=List[ContactResponse], dependencies=[Depends(allowed_operation_get)],
+    response_model=List[ContactResponse], dependencies=[
+        Depends(allowed_operation_get)],
     name="Search contacts by email",
 )
 async def search_contacts_by_email(email: str, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.search_contact_by_email(email, db)
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return contact
 
 
@@ -83,7 +90,8 @@ async def create_contacts(body: ContactBase, db: Session = Depends(get_db), curr
 async def update_contact(body: ContactBase, id: int, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.update(id, body, db)
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
 
     return contact
 
@@ -92,7 +100,8 @@ async def update_contact(body: ContactBase, id: int, db: Session = Depends(get_d
 async def remove_contact(id: int, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.remove(id, db)
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return contact
 
 
@@ -104,5 +113,6 @@ async def get_birthdays(db: Session = Depends(get_db), current_user: User = Depe
     end_date = today + timedelta(days=7)
     birthdays = await repository_contacts.get_birthdays(today, end_date, db)
     if birthdays is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return birthdays
